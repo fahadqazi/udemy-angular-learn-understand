682b41bee10292153e3b3ebdcb86bfd9d9f81053
Fahad Qazi <fahadqazi@gmail.com>
2016-04-01 20:29:42 +0100

    data binding and Directives- Section 4

--- Section One/app.js
+++ Section One/app.js
@@ -4,16 +4,23 @@ var angularApp = angular.module('angularApp', []);
 // CONTROLLERS
 angularApp.controller('mainController', ['$scope','$filter', function($scope,$filter) {
     
-    $scope.name = "Fahad Qazi";
-    $scope.occupation = "Coder";
-    $scope.bio = function(){
-      console.log($scope.name + ": " + $scope.occupation);
-    };
-  
+    $scope.handle = '';
+    $scope.characters = 5;  
     
     $scope.lowercasehandle = function(){
       return $filter('lowercase')($scope.handle);
     }
+    
+    $scope.rules = [
+      
+      {rulename: "Must be 5 characters"},
+      {rulename: "Must not be used elesewhere"},
+      {rulename: "Must be cool"}
+    ];
+  
+    $scope.alertme = function(){
+        alert("Alert Clicked!");
+    };
   
 }]);
 
